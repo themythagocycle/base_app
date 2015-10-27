8010b3db6df4b1d59bbbe0730727c8889ed1141b
Nathan Austin <themythagocycle@gmail.com>
2015-10-27 13:26:15 -0400

    fixed css config issue, rake test issue remains

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "get home" do
+    get :home
+    assert_response :success
+  end
+
 end
