e8b589b82fde46893317c33f9fd85869c18f2f14
Nathan Austin <themythagocycle@gmail.com>
2015-10-28 19:20:38 -0400

    signup redirect working again

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,8 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    'dashboard/index' # Or :prefix_to_your_route
+    '/dashboard/index' # Or :prefix_to_your_route
   end
-
-
 end
