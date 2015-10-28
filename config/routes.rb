8a86459891dc9d2e9ea81ac04524b2ef074e7b77
Nathan Austin <themythagocycle@gmail.com>
2015-10-28 19:27:20 -0400

    changed devise to login logout from sign_in sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, controllers: { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
